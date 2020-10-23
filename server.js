cc6c7aacb4e388baa21501e4d83d73979a6e755f
minhthu <minhthu512586@gmail.com>
2020-10-23 08:14:40 +0700

    make it better

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
 const app = express()
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+// app.use('/', serveStatic(path.join(__dirname, '/dist')))
 const port = process.env.PORT || 8080
 app.listen(port)
