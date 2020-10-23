496c5fe4da2f8d32a3190642b88f03a1cdca1c37
minhthu <minhthu512586@gmail.com>
2020-10-23 08:20:34 +0700

    make it better

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-if (process.env.NODE_ENV === "production"{
-    app.use(express.static("build"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-    });
-  }
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
 const app = express()
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static("build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+    });
+  }
 const port = process.env.PORT || 8080
 app.listen(port)
