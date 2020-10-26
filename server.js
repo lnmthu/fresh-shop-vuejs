56a03ea9a575c1bff4468413abc7ef4127ac62f5
minhthu <minhthu512586@gmail.com>
2020-10-26 09:08:29 +0700

    Adding files.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
+const routes = require('./src/routes')
 const app = express()
-app.use('/', serveStatic(path.join(__dirname, '/dist/frontend')))
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.use(routes)
 const port = process.env.PORT || 8080
 app.listen(port)
