baf0bc0a2e6526fce18b54b05683c52383f1502c
le thu <minhthu512586@gmail.com>
2021-11-11 09:07:41 +0700

    a

--- src/views/Home/components/FeaturedProduct.vue
+++ src/views/Home/components/FeaturedProduct.vue
@@ -24,17 +24,7 @@
               class="featured__item__pic set-bg"
               :style="{ backgroundImage: 'url(' + product.image_uri + ')' }"
             >
-              <ul class="featured__item__pic__hover">
-                <li>
-                  <a href="#"><i class="fa fa-heart"></i></a>
-                </li>
-                <li>
-                  <a href="#"><i class="fa fa-retweet"></i></a>
-                </li>
-                <li>
-                  <a href="#"><i class="fa fa-shopping-cart"></i></a>
-                </li>
-              </ul>
+             
             </div>
             <div class="featured__item__text">
               <h6>
