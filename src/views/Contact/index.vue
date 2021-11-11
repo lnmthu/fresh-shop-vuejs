529d562e1382a51c897d322ff1356b6d8d001bf0
le thu <minhthu512586@gmail.com>
2021-11-11 09:58:33 +0700

    a

--- src/views/Contact/index.vue
+++ src/views/Contact/index.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
     <Hero />
+       <div class="banner">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 col-md-6 col-sm-6">
+            <div class="banner__pic">
+              <img src="dist/img/banner/banner-1.jpg" alt="" />
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-6">
+            <div class="banner__pic">
+              <img src="dist/img/banner/banner-2.jpg" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <BreadCrumb />
     <Contact/>
     <Map/>
